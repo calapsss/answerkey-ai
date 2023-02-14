cc79d1a55c9fb30200ee9e9da457df28dea71d4c
Charles Calapini <7816568+calapsss@users.noreply.github.com>
2023-02-14 00:58:10 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import { Text, Page } from '@vercel/examples-ui'
 import { Chat } from '../components/Chat'
+import Link from 'next/link'
 
 function Home() {
   return (
     <Page className="flex flex-col gap-12">
       <section className="flex flex-col gap-6">
-        <Text variant="h1">Answerkey.info</Text>
+        <Text variant="h1">Answerkey.info Assistant</Text>
         <Text className="text-zinc-600">
           Your teacher banned chat-gpt? No problem, we have a solution for you! Talk to our own AI chat bot and get your answers!
         </Text>
@@ -16,6 +17,10 @@ function Home() {
         <div className="lg:w-2/3">
           <Chat />
         </div>
+      </section>
+
+      <section className="flex flex-col gap-3">
+        <Text variant="text-zinc-600">Created by <Link href='https://medium.com/@pipsworld'>Charles Calapini</Link></Text>
       </section>
     </Page>
   )
